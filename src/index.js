b3f6f6adcd4c00e9ef5b590eba6f940f136173f6
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-09-20 21:46:03 +0300

    Add Express Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("test");
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('It Works');
+});
+
+app.listen(3300, () => console.log('Server is listening on http://localhost:3300...'));
