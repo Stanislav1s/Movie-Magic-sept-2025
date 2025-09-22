3b9c463ad621447baef82cd218e4e7307e132c96
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-09-22 23:11:33 +0300

    Add movie controller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import homeController from './controllers/homeController.js';
+import movieController from './controllers/movieController.js';
 
 const app = express();
 
@@ -15,5 +16,6 @@ app.set('views', 'src/views');
 app.use(express.static('src/public'))
 
 app.use(homeController);
+app.use(movieController);
 
 app.listen(3300, () => console.log('Server is listening on http://localhost:3300...'));
